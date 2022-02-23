855e62f2e6692014c36d1661ac19441211e163a9
Meer Abdullah <96878742+MeerAbdullah@users.noreply.github.com>
2022-02-23 06:12:43 -0500

    Add files via upload
    Crazy 8's Card Game implementation!

---
+++ Crazy8s/src/Main.java
@@ -0,0 +1,11 @@
+
+/**
+ * @author Meer Abdullah
+ * @date April 3rd, 2021
+ * @synopsis Main method which starts the CrazyEights card game.
+ */
+public class Main {
+	public static void main(String[] args) {
+		new CrazyEights(new Deck());
+	}
+}
